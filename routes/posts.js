a68818a982e81191ebd36d38291fb82eb31acc45
Hudson <hjp223@lehigh.edu>
2021-06-09 11:23:02 -0400

    refactored the post.js routes to use async await

--- routes/posts.js
+++ routes/posts.js
@@ -1,25 +1,29 @@
 const express = require("express");
 const router = express.Router(); //can use the ROUTER to make routes instead of the application. helps with modulating
 const Post = require("../models/Post");
-router.get("/", (req, res) => {
-  res.send("We are on posts");
+//Gets back all the posts at user/posts
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
-
+//Submits a post
 router.post("/", async (req, res) => {
   const post = new Post({
     //create a new entry for the Post table we created
     title: req.body.title, //get title from the request body
     description: req.body.description,
   });
-  const savedPost = await post // send the post to the database with post.save()
-    .save()
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((err) => {
-      //error check
-      res.json({ message: err });
-    });
+  try {
+    const savedPost = await post.save(); // send the post to the database with post.save()
+    res.json(savedPost);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
+//fetch specific post
 
 module.exports = router;
